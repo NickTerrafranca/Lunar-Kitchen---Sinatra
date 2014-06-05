7737cc970361300184438d2cb828914d824ed461
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-05 19:45:36 -0400

    Initial commit!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,33 @@
 class Ingredient
+  attr_reader :ingredient, :name
+
+  def initialize(name)
+    @ingredient = ingredient
+    @name = name
+  end
+
+  def self.db_connection
+    begin
+      connection = PG.connect(dbname: 'recipes')
+      yield(connection)
+    ensure
+      connection.close
+    end
+  end
+
+  def self.ingredients(recipe_id)
+    all_ingredients = []
+    query = "SELECT ingredients.name
+    FROM ingredients
+    JOIN recipes
+    ON ingredients.recipe_id = recipes.id
+    WHERE recipe_id = $1;"
+    ingredients = db_connection do |conn|
+      conn.exec_params(query, [recipe_id])
+    end
+    ingredients.each do |ingredient|
+      all_ingredients << Ingredient.new(ingredient['name'])
+    end
+    all_ingredients
+  end
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,62 @@
+require_relative 'ingredient'
+
 class Recipe
+  attr_reader :id, :name, :instructions, :description
+
+  def initialize(id, name, instructions = nil, description = nil)
+    @id = id
+    @name = name
+
+    if instructions == nil
+      @instructions = "This recipe doesn't have any instructions."
+    else
+      @instructions = instructions
+    end
+
+    if description == nil
+      @description = "This recipe doesn't have a description."
+    else
+      @description = description
+    end
+  end
+
+  def ingredients
+    Ingredient.ingredients(id)
+  end
+
+  def self.db_connection
+    begin
+      connection = PG.connect(dbname: 'recipes')
+      yield(connection)
+    ensure
+      connection.close
+    end
+  end
+
+  def self.all
+    all_recipes = []
+    query = "SELECT recipes.id, recipes.name, recipes.instructions, recipes.description
+    FROM recipes
+    ORDER BY recipes.name;"
+    recipes = db_connection do |conn|
+      conn.exec_params(query)
+    end
+    recipes.each do |recipe|
+      all_recipes << Recipe.new(recipe['id'], recipe['name'], recipe['instructions'], recipe['description'])
+    end
+    all_recipes
+  end
+
+
+  def self.find(recipe_id)
+    query = "SELECT recipes.id, recipes.name, recipes.instructions, recipes.description
+    FROM recipes
+    JOIN ingredients
+    ON ingredients.recipe_id = recipes.id
+    WHERE recipe_id = $1;"
+    recipe_details = db_connection do |conn|
+      conn.exec_params(query, [recipe_id])
+    end
+    Recipe.new(recipe_details[0]['id'], recipe_details[0]['name'], recipe_details[0]['instructions'], recipe_details[0]['description'])
+  end
 end
